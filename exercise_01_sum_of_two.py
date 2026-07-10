a3899577b0f440aea372ddebd731908318a28170
So Monyratanak <nak087ziin@gmail.com>
2026-07-10 20:06:10 +0700

    Excercise 01_02

---
+++ exercise_01_sum_of_two.py
@@ -0,0 +1,6 @@
+a = 15
+b = 27
+
+total = a + b
+
+print(f"The sum of {a} and {b} is {total}")
---
+++ exercise_02_age_calculator.py
@@ -0,0 +1,6 @@
+birth_year = 1998
+current_year = 2026
+
+age = current_year - birth_year
+
+print(f"If you were born in {birth_year}, you are {age} years old in {current_year}.")
