6ca515ee72de47182c8f59daea6071641f77b7c9
Student Buth Chroesna <buthchroesna23@mekong.edu.kh>
2026-07-10 20:07:05 +0700

    Added exercise 5 - 6

---
+++ exercise_05_square_area.py
@@ -0,0 +1,5 @@
+side_length = 8
+
+area = side_length**2
+
+print(f"A square with side length {side_length} has an area of {area}")
---
+++ exercise_06_fair_bill_split.py
@@ -0,0 +1,7 @@
+total_bill = 95.50
+friends = 4
+
+amount_per_person = total_bill / friends
+
+print(f"A ${total_bill} bill split among {friends} friends.")
+print(f"Each person owes ${amount_per_person:.2f}")
